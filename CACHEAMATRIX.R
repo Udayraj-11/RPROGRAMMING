094cfd26e1fe6d7714f3d3b176c518609cb55baa
Udayraj-11 <70016050+Udayraj-11@users.noreply.github.com>
2020-09-09 18:00:58 +0530

    Add files via upload

---
+++ CACHEAMATRIX.R
@@ -0,0 +1,37 @@
+makeCacheMatrix - function(x = matrix()) {
+        
+        inv = NULL
+        set = function(y) {
+                # use `-` to assign a value to an object in an environment 
+                # different from the current environment. 
+                x - y
+                inv - NULL
+        }
+        get = function() x
+        setinv = function(inverse) inv - inverse 
+        getinv = function() inv
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
+}
+
+cacheSolve - function(x, ...) {
+        ## @x output of makeCacheMatrix()
+        ## return inverse of the original matrix input to makeCacheMatrix()
+        
+        inv = x$getinv()
+        
+        # if the inverse has already been calculated
+        if (!is.null(inv)){
+                # get it from the cache and skips the computation. 
+                message(getting cached data)
+                return(inv)
+        }
+        
+        # otherwise, calculates the inverse 
+        mat.data = x$get()
+        inv = solve(mat.data, ...)
+        
+        # sets the value of the inverse in the cache via the setinv function.
+        x$setinv(inv)
+        
+        return(inv)
+}
